f41606d575fd896b1d51c5320fd7f56d70b3a936
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-03 20:54:15 -0300

    [03/03/2023] Atividade EtecAB

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string frase = "Etec Adolpho Berezin";
+Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Cyan;
+for (int i = 0; i < frase.Length; i++)
+{
+    Console.Write(frase[i]);
+    Thread.Sleep(100);
+}
+Console.ResetColor();
